f9ce1301ef2edbf6b7b6602cff35b84e520e73d4
Josh Benham <joshbenham@gmail.com>
2016-05-09 20:31:35 +1000

    Added sublime text 3 to installer

--- ubuntu/packages.sh
+++ ubuntu/packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-read -p " <- Do you want to update the Repositories? [Y|n] " -r
+read -p " <- Do you want to update the Repositories? [y|n] " -r
 echo
 
 if [[ $REPLY =~ ^[Yy]$ ]] ; then
@@ -9,10 +9,16 @@ fi
 
 echo " ->Installing GNU Stow"
 echo
-
 sudo apt-get install stow
+
+
 
 echo " -> Installing Firefox Nightly"
 echo
-
 sudo apt-get install firefox-trunk
+
+
+
+echo " -> Installing Sublime Text 3"
+echo
+sudo apt-get install sublime-text-installer
--- ubuntu/repositories.sh
+++ ubuntu/repositories.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-read -p " <- Do you want to install the Firefox Nightly repository? [Y|n] " -r
+read -p " <- Do you want to install the Firefox Nightly repository? [y|n] " -r
 echo
 
 if [[ $REPLY =~ ^[Yy]$ ]] ; then
     sudo add-apt-repository ppa:ubuntu-mozilla-daily/ppa
+fi
+
+read -p " <- Do you want to install the Sublime Text 3 repository? [y|n] " -r
+echo
+
+if [[ $REPLY =~ ^[Yy]$ ]] ; then
+    sudo add-apt-repository ppa:webupd8team/sublime-text-3
 fi
