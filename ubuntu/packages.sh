32acb41d7b47fb55d871c66b9e86b45887dbf158
Josh Benham <joshbenham@gmail.com>
2016-05-09 20:41:58 +1000

    Added dropbox into the installer

--- ubuntu/packages.sh
+++ ubuntu/packages.sh
@@ -10,15 +10,27 @@ fi
 echo " ->Installing GNU Stow"
 echo
 sudo apt-get install stow
+echo
 
 
 
 echo " -> Installing Firefox Nightly"
 echo
 sudo apt-get install firefox-trunk
+echo
 
 
 
 echo " -> Installing Sublime Text 3"
 echo
 sudo apt-get install sublime-text-installer
+echo
+
+
+
+echo " -> Installing Dropbox"
+echo
+if [ -f ~/dropbox-dist/dropboxd ] ; then
+	cd ~ && wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
+fi
+echo
