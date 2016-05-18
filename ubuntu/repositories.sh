96d7e4293e733c05b96d429f711785e4696daa18
Josh Benham <joshbenham@gmail.com>
2016-05-19 00:02:20 +1000

    Added atom repository

--- ubuntu/repositories.sh
+++ ubuntu/repositories.sh
@@ -7,6 +7,13 @@ if [[ $REPLY =~ ^[Yy]$ ]] ; then
     sudo add-apt-repository ppa:ubuntu-mozilla-daily/ppa
 fi
 
+read -p " <- Do you want to install the Atom Editor repository? [y|n] " -r
+echo
+
+if [[ $REPLY =~ ^[Yy]$ ]] ; then
+    sudo add-apt-repository ppa:webupd8team/atom
+fi
+
 read -p " <- Do you want to install the Sublime Text 3 repository? [y|n] " -r
 echo
 
