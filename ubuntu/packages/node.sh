c52196815a6bf3ce3a5f63f1e4d0e0f858442a26
Josh Benham <joshbenham@gmail.com>
2016-05-19 11:47:31 +1000

    Symlinked nodejs to node and add gulp/bower to install

--- ubuntu/packages/node.sh
+++ ubuntu/packages/node.sh
@@ -1,4 +1,4 @@
-echo " -> Installing Node Packages"
+echo " -> Installing Node Aptitude Packages"
 echo
 packagelist=(
 
@@ -9,4 +9,24 @@ nodejs
 npm
 )
 sudo apt-get install ${packagelist[@]}
+echo
+
+
+echo " -> Mapping nodejs to node"
+echo
+sudo ln -sf /usr/bin/nodejs /usr/bin/node
+echo
+
+
+echo " -> Installing Node Packages"
+echo
+packagelist=(
+
+# TOOLS
+# ------------------------
+
+gulp
+bower
+)
+sudo npm -g install ${packagelist[@]}
 echo
