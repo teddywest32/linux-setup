8697706640fb48533eb914f389c39dcfb6c90193
Josh Benham <joshbenham@gmail.com>
2016-05-23 15:01:04 +1000

    Added sublime settings to the mix

--- ubuntu/packages/aptitude.sh
+++ ubuntu/packages/aptitude.sh
@@ -84,9 +84,23 @@ headline " -> Mapping PHP5.6 to PHP"
 
 sudo ln -sf /usr/bin/php5.6 /etc/alternatives/php
 
-
-if [[ -f ~/dropbox-dist/dropboxd ]] ; then
+FILENAME="$HOME/.dropbox-dist/dropboxd"
+if [[ ! -f "$FILENAME" ]] ; then
     headline " -> Installing Dropbox"
     cd ~ && wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
     echo
 fi
+
+
+headline " -> Setting up Sublime Text 3 packages"
+
+DIRECTORY="$HOME/.config/sublime-text-3/Installed Packages"
+FILENAME="$DIRECTORY/Package Control.sublime-package"
+if [[ ! -f "$FILENAME" ]] ; then
+	cd "$DIRECTORY"
+	wget "http://sublime.wbond.net/Package%20Control.sublime-package" -O "$FILENAME"
+fi
+
+DIRECTORY="$HOME/.config/sublime-text-3/Packages/User/"
+ln -sf "$HOME/Code/Personal/linux-setup/data/sublime-text-3/Package Control.sublime-settings" "$DIRECTORY/Package Control.sublime-settings"
+ln -sf "$HOME/Code/Personal/linux-setup/data/sublime-text-3/Preferences.sublime-settings" "$DIRECTORY/Preferences.sublime-settings"
