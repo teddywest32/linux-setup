37ae55da79b4596adaf29973e599ec0267548a43
Josh Benham <joshbenham@gmail.com>
2016-05-31 17:08:39 +1000

    Changed the store directory

--- ubuntu/directories.sh
+++ ubuntu/directories.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ask_question " <- Have you installed Dropbox and would you like to setup Code folder [y|n] "
+ask_question " <- Would you like to setup Code folder [y|n] "
 echo
 
 if said_yes ; then
@@ -9,11 +9,22 @@ if said_yes ; then
     $COMMAND
 fi
 
-ask_question " <- Have you installed Dropbox and would you like to setup Applications folder [y|n] "
+
+ask_question " <- Would you like to setup Applications folder [y|n] "
 echo
 
 if said_yes ; then
     NAME=`whoami`
     COMMAND="ln -sf /media/$NAME/Data/Applications /home/$NAME/Applications"
+    $COMMAND
+fi
+
+
+ask_question " <- Would you like to setup Password Store folder [y|n] "
+echo
+
+if said_yes ; then
+    NAME=`whoami`
+    COMMAND="ln -sf /media/$NAME/Data/PassStore /home/$NAME/.password-store"
     $COMMAND
 fi
