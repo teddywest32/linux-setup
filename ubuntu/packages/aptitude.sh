cfa7c00e10ad729d912a97fe8183d8f2a3ffb2bb
Josh Benham <joshbenham@gmail.com>
2016-06-01 18:41:58 +1000

    Organising php packages

--- ubuntu/packages/aptitude.sh
+++ ubuntu/packages/aptitude.sh
@@ -64,29 +64,29 @@ mysql-client
 
 php-gettext
 
-php7.0-mysql
+php7.0-cgi
 php7.0-curl
+php7.0-gd
 php7.0-json
 php7.0-mbstring
 php7.0-mcrypt
+php7.0-mysql
 php7.0-sqlite3
-php7.0-gd
 php7.0-xml
 php7.0-zip
-php7.0-cgi
 php7.0
 libapache2-mod-php7.0
 
-php5.6-mysql
+php5.6-cgi
 php5.6-curl
+php5.6-gd
 php5.6-json
 php5.6-mbstring
 php5.6-mcrypt
+php5.6-mysql
 php5.6-sqlite3
-php5.6-gd
 php5.6-xml
 php5.6-zip
-php5.6-cgi
 php5.6
 libapache2-mod-php5.6
 
